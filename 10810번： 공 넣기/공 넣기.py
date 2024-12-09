dbb28bd351bce8d6ddfabf5244936871bdbbe725
sungbin <dusvlf5950@naver.com>
2024-12-09 14:36:03 +0900

    1209

--- 10810번： 공 넣기/공 넣기.py
+++ 10810번： 공 넣기/공 넣기.py
@@ -15,4 +15,4 @@ for i in range (m):
     a, b, c = map(int, input().split())
     for j in range(a-1, b):
         basket[j] = c
-print(' '.join(map(str, basket)))
+print(' '.join(map(str, basket))) 
